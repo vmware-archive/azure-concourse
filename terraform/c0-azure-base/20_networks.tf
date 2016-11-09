385b32acc80a1954a2177b2c06df2a3444027714
Ford Donald <fdonald@pivotal.io>
2016-11-09 12:17:22 -0600

    Terraform Network Updates

--- terraform/c0-azure-base/20_networks.tf
+++ terraform/c0-azure-base/20_networks.tf
@@ -7,7 +7,7 @@ resource "azurerm_virtual_network" "pcf_virtual_network" {
   name                = "${var.env_name}-virtual-network"
   depends_on          = ["azurerm_resource_group.pcf_resource_group"]
   resource_group_name = "${azurerm_resource_group.pcf_resource_group.name}"
-  address_space       = ["10.0.0.0/16"]
+  address_space       = ["192.168.0.0/16"]
   location            = "${var.location}"
 }
 
@@ -16,7 +16,7 @@ resource "azurerm_subnet" "opsman_and_director_subnet" {
   depends_on           = ["azurerm_resource_group.pcf_resource_group"]
   resource_group_name  = "${azurerm_resource_group.pcf_resource_group.name}"
   virtual_network_name = "${azurerm_virtual_network.pcf_virtual_network.name}"
-  address_prefix       = "10.0.8.0/26"
+  address_prefix       = "192.168.100.0/26"
 }
 
 resource "azurerm_subnet" "ert_subnet" {
@@ -24,13 +24,13 @@ resource "azurerm_subnet" "ert_subnet" {
   depends_on           = ["azurerm_resource_group.pcf_resource_group"]
   resource_group_name  = "${azurerm_resource_group.pcf_resource_group.name}"
   virtual_network_name = "${azurerm_virtual_network.pcf_virtual_network.name}"
-  address_prefix       = "10.0.0.0/22"
+  address_prefix       = "192.168.8.0/22"
 }
 
 resource "azurerm_subnet" "services_subnet" {
-  name                 = "${var.env_name}-services-subnet"
+  name                 = "${var.env_name}-services-01-subnet"
   depends_on           = ["azurerm_resource_group.pcf_resource_group"]
   resource_group_name  = "${azurerm_resource_group.pcf_resource_group.name}"
   virtual_network_name = "${azurerm_virtual_network.pcf_virtual_network.name}"
-  address_prefix       = "10.0.4.0/22"
+  address_prefix       = "192.168.12.0/22"
 }
