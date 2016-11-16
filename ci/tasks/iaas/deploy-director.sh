8b8f538a55b63cb410a2665a54d9f2602b761c98
virtmerlin <merlin@sense.net>
2016-11-16 15:09:59 -0800

    regex mod ssh key cert

--- ci/tasks/iaas/deploy-director.sh
+++ ci/tasks/iaas/deploy-director.sh
@@ -13,6 +13,8 @@ json_file_path="azure-concourse/json-opsman/${azure_pcf_terraform_template}"
 json_file_template="${json_file_path}/opsman-template.json"
 json_file="${json_file_path}/opsman.json"
 
+pcf_ssh_key_priv=$(echo ${pcf_ssh_key_priv} | sed 's/\s\+/\\\\r\\\\n/g' | sed 's/\\\\r\\\\nCERTIFICATE/ CERTIFICATE/g')
+
 cp ${json_file_template} ${json_file}
 
 perl -pi -e "s|{{infra_subnet_iaas}}|${azure_terraform_prefix}-virtual-network/${azure_terraform_prefix}-opsman-and-director-subnet|g" ${json_file}
