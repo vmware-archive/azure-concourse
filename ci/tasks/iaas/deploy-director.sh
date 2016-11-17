eb155256dc1af841abae1a3e1f24eea476dff8af
virtmerlin <merlin@sense.net>
2016-11-16 22:49:16 -0800

    regex mod ssh key cert

--- ci/tasks/iaas/deploy-director.sh
+++ ci/tasks/iaas/deploy-director.sh
@@ -41,7 +41,7 @@ perl -pi -e "s|{{services1_subnet_gateway}}|${azure_terraform_subnet_services1_g
 
 # Apply Changes in Opsman
 
-#om-linux --target https://opsman.$pcf_ert_domain -k \
-#       --username "$pcf_opsman_admin" \
-#       --password "$pcf_opsman_admin_passwd" \
-#  apply-changes
+om-linux --target https://opsman.$pcf_ert_domain -k \
+       --username "$pcf_opsman_admin" \
+       --password "$pcf_opsman_admin_passwd" \
+  apply-changes
