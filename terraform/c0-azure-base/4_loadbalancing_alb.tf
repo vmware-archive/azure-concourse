009dbffc62c361fcb014e00addc9fa1e89a8be77
Rahul Jain <rjain@pivotal.io>
2016-12-06 15:19:00 -0600

    Cleanup

--- terraform/c0-azure-base/4_loadbalancing_alb.tf
+++ terraform/c0-azure-base/4_loadbalancing_alb.tf
@@ -4,6 +4,7 @@
 ///////////////////////////////////////////////
 
 
+// API/APPS Controller traffic
 resource "azurerm_lb" "web" {
   name                = "${var.env_name}-web-lb"
   location            = "${var.location}"
@@ -15,11 +16,30 @@ resource "azurerm_lb" "web" {
   }
 }
 
+// TCP traffic
+resource "azurerm_lb" "tcp" {
+  name                = "${var.env_name}-tcp-lb"
+  location            = "${var.location}"
+  resource_group_name = "${var.env_name}"
+
+  frontend_ip_configuration = {
+    name                 = "frontendip"
+    public_ip_address_id = "${var.pub_ip_id_tcp_lb}"
+  }
+}
+
 resource "azurerm_lb_backend_address_pool" "web-backend-pool" {
   name                = "web-backend-pool"
   location            = "${var.location}"
   resource_group_name = "${var.env_name}"
   loadbalancer_id     = "${azurerm_lb.web.id}"
+}
+
+resource "azurerm_lb_backend_address_pool" "tcp-backend-pool" {
+  name                = "tcp-backend-pool"
+  location            = "${var.location}"
+  resource_group_name = "${var.env_name}"
+  loadbalancer_id     = "${azurerm_lb.tcp.id}"
 }
 
 resource "azurerm_lb_probe" "web-https-probe" {
@@ -29,6 +49,15 @@ resource "azurerm_lb_probe" "web-https-probe" {
   loadbalancer_id     = "${azurerm_lb.web.id}"
   protocol            = "TCP"
   port                = 443
+}
+
+resource "azurerm_lb_probe" "tcp-probe" {
+  name                = "tcp-probe"
+  location            = "${var.location}"
+  resource_group_name = "${var.env_name}"
+  loadbalancer_id     = "${azurerm_lb.tcp.id}"
+  protocol            = "TCP"
+  port                = 80
 }
 
 resource "azurerm_lb_rule" "web-https-rule" {
@@ -95,4 +124,21 @@ resource "azurerm_lb_rule" "web-ssh-rule" {
   # Workaround until the backend_address_pool and probe resources output their own ids
   backend_address_pool_id = "${azurerm_lb.web.id}/backendAddressPools/${azurerm_lb_backend_address_pool.web-backend-pool.name}"
   probe_id                = "${azurerm_lb.web.id}/probes/${azurerm_lb_probe.web-ssh-probe.name}"
+}
+
+resource "azurerm_lb_rule" "tcp-rule" {
+  count               = 150
+  name                = "tcp-rule-${count.index + 1024}"
+  location            = "${var.location}"
+  resource_group_name = "${var.env_name}"
+  loadbalancer_id     = "${azurerm_lb.tcp.id}"
+
+  frontend_ip_configuration_name = "frontendip"
+  protocol                       = "TCP"
+  frontend_port                  = "${count.index + 1024}"
+  backend_port                   = "${count.index + 1024}"
+
+  # Workaround until the backend_address_pool and probe resources output their own ids
+  backend_address_pool_id = "${azurerm_lb.tcp.id}/backendAddressPools/${azurerm_lb_backend_address_pool.tcp-backend-pool.name}"
+  probe_id                = "${azurerm_lb.tcp.id}/probes/${azurerm_lb_probe.tcp-probe.name}"
 }
