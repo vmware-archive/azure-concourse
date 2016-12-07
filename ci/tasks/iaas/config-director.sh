fdd8c35e0478601fbbd8d66d00bf835e02067992
virtmerlin <merlin@sense.net>
2016-12-07 15:53:52 -0600

    Added Dynamic Services Subnet to Opsman Director config

--- ci/tasks/iaas/config-director.sh
+++ ci/tasks/iaas/config-director.sh
@@ -29,6 +29,11 @@ perl -pi -e "s|{{services1_subnet_cidr}}|${azure_terraform_subnet_services1_cidr
 perl -pi -e "s|{{services1_subnet_reserved}}|${azure_terraform_subnet_services1_reserved}|g" ${json_file}
 perl -pi -e "s|{{services1_subnet_dns}}|${azure_terraform_subnet_services1_dns}|g" ${json_file}
 perl -pi -e "s|{{services1_subnet_gateway}}|${azure_terraform_subnet_services1_gateway}|g" ${json_file}
+perl -pi -e "s|{{dynamic_services_subnet_iaas}}|${azure_terraform_prefix}-virtual-network/${azure_terraform_prefix}-dynamic-services-subnet|g" ${json_file}
+perl -pi -e "s|{{dynamic_services_subnet_cidr}}|${azure_terraform_subnet_dyanmic_services_cidr}|g" ${json_file}
+perl -pi -e "s|{{dynamic_services_subnet_reserved}}|${azure_terraform_subnet_dyanmic_services_reserved}|g" ${json_file}
+perl -pi -e "s|{{dynamic_services_subnet_dns}}|${azure_terraform_subnet_dyanmic_services_dns}|g" ${json_file}
+perl -pi -e "s|{{dynamic_services_subnet_gateway}}|${azure_terraform_subnet_dyanmic_services_gateway}|g" ${json_file}
 
 
 
